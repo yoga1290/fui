87537c72c97074d78bca8f74eebd5f3d3225ed7e
Youssef Gamil <yoga1290@gmail.com>
2018-06-19 22:14:42 +0200

    b

--- main.bundle.js
+++ main.bundle.js
@@ -155,8 +155,11 @@ var DrawerComponent = /** @class */ (function () {
     DrawerComponent.prototype.ngAfterViewInit = function () {
         console.log(__WEBPACK_IMPORTED_MODULE_1_material_components_web__);
         var MDCTemporaryDrawer = __WEBPACK_IMPORTED_MODULE_1_material_components_web__["drawer"].MDCTemporaryDrawer;
-        var _drawer = new MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
-        document.querySelector('.menu').addEventListener('click', function () { return _drawer.open = true; });
+        this._drawer = new MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
+        // document.querySelector('.menu').addEventListener('click', () => _drawer.open = true);
+    };
+    DrawerComponent.prototype.openDrawer = function () {
+        this._drawer.open != this._drawer.open;
     };
     DrawerComponent = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
