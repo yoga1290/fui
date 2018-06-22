97517eda4d6f6982a0d0b562e4351805b83fbcd0
Youssef Gamil <yoga1290@gmail.com>
2018-06-22 22:07:54 +0200

    b

--- main.bundle.js
+++ main.bundle.js
@@ -452,7 +452,7 @@ var ImportToolComponent = /** @class */ (function () {
 /***/ "./src/app/overlay/overlay.component.pug":
 /***/ (function(module, exports) {
 
-module.exports = "<div id=\"overlay\"><div class=\"overlay-layer\"><button class=\"close mdc-button\" (click)=\"close.emit()\"><i class=\"material-icons mdc-button__icon\" aria-hidden=\"true\">close</i>close</button><div class=\"event\" *ngFor=\"let n of data.notes\">{{ n.text }}</div><button class=\"mdc-button\"><i class=\"material-icons mdc-button__icon\" aria-hidden=\"true\">add</i>Note</button><div class=\"mdc-card\" [id]=\"elId\" [hidden]=\"true\"><div class=\"mdc-card__media mdc-card__media--square\"><div class=\"mdc-card__media-content\"><div class=\"mdc-typography--headline6\">Title</div><div class=\"mdc-typography--subtitle2\">subtitle</div><div class=\"mdc-typography--body2\" *ngFor=\"let n of data.notes\">{{ n.text }}</div></div></div><div class=\"mdc-card__actions\"><div class=\"mdc-card__action-buttons\"><button class=\"mdc-button mdc-card__action mdc-card__action--button\">Action</button></div><div class=\"mdc-card__action-icons\"><i class=\"material-icons mdc-card__action mdc-card__action--icon\">share</i></div></div></div></div></div>"
+module.exports = "<div id=\"overlay\" [id]=\"elId\"><div class=\"overlay-layer\"><button class=\"close mdc-button\" (click)=\"close.emit()\"><i class=\"material-icons mdc-button__icon\" aria-hidden=\"true\">close</i>close</button><div class=\"event\" *ngFor=\"let n of data.notes\">{{ n.text }}</div><div class=\"mdc-text-field mdc-text-field--outlined\"><input class=\"mdc-text-field__input\" type=\"text\" [id]=\"labelId\" [(ngModel)]=\"notes\"/><label class=\"mdc-floating-label\" [for]=\"labelId\">Note</label><div class=\"mdc-notched-outline\"><svg><path class=\"mdc-notched-outline__path\"></path></svg></div><div class=\"mdc-notched-outline__idle\"></div></div><button class=\"mdc-button\" (click)=\"onNewNote.emit(notes)\"><i class=\"material-icons mdc-button__icon\" aria-hidden=\"true\">add</i>Note</button><div class=\"mdc-card\" [hidden]=\"true\"><div class=\"mdc-card__media mdc-card__media--square\"><div class=\"mdc-card__media-content\"><div class=\"mdc-typography--headline6\">Title</div><div class=\"mdc-typography--subtitle2\">subtitle</div><div class=\"mdc-typography--body2\" *ngFor=\"let n of data.notes\">{{ n.text }}</div></div></div><div class=\"mdc-card__actions\"><div class=\"mdc-card__action-buttons\"><button class=\"mdc-button mdc-card__action mdc-card__action--button\">Action</button></div><div class=\"mdc-card__action-icons\"><i class=\"material-icons mdc-card__action mdc-card__action--icon\">share</i></div></div></div></div></div>"
 
 /***/ }),
 
@@ -495,8 +495,8 @@ var OverlayComponent = /** @class */ (function () {
     OverlayComponent.prototype.ngAfterViewInit = function () {
         var MDCTextField = __WEBPACK_IMPORTED_MODULE_1_material_components_web__["textField"].MDCTextField;
         var MDCNotchedOutline = __WEBPACK_IMPORTED_MODULE_1_material_components_web__["notchedOutline"].MDCNotchedOutline;
-        var textField_videoOverlay = new MDCTextField(document.querySelector("#" + this.elId + " .mdc-text-field.video-overlay"));
-        var notchedOutline_videoOverlay = new MDCNotchedOutline(document.querySelector("#" + this.elId + " .mdc-text-field.video-overlay .mdc-notched-outline"));
+        var textField_videoOverlay = new MDCTextField(document.querySelector("#" + this.elId + " .mdc-text-field"));
+        var notchedOutline_videoOverlay = new MDCNotchedOutline(document.querySelector("#" + this.elId + " .mdc-text-field .mdc-notched-outline"));
     };
     __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["z" /* Input */])("data"),
